cca13c7e38f83f1edbd9e843424605c13ac31d1e
TraciClymer <traciclymer@gmail.com>
2016-10-06 16:15:18 -0400

    Assignment 2
    Assignment 2: write a pair of functions that read in a square matrix
    and calculate or retrieve its inverse from the cache

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The functions makeCacheMatrix and cacheSolve take a square matrix as input, calculates its inverse and caches the value
 
-## Write a short comment describing this function
+## makeCacheMatrix creates a list containing functions to set the value of the vector, get the value of a vector, set the value of the mean, and get the value of the mean
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        i <- NULL   #initialize i for inverse
+        set <- function(y) {
+                x <<- y    #assign input to object x in parent environment
+                i <<-NULL  #assign NULL to object i in parent environment
+        }
+        get <- function() x   #retrieves the value of x from the parent environment
+        setInverse <- function(inverse) i <<- inverse   #assign input to i from parent environment
+        getInverse <- function() i   #retrieves the value of x 
+        list(set = set,   
+             get = get,
+             setInverse = setInverse,
+             getInverse = getInverse) #assigns each function as an element in a list
 }
 
 
-## Write a short comment describing this function
+## cacheSolve computes the inverse of the matrix returned by makeCacheMatrix, if the inverse has already been calculated it should retrieve the inverse from the cache
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        i <- x$getInverse() #retrieve values from makeCacheMatrix()
+        if (!is.null(i)) {
+                message("getting cached data")
+                return(i)
+        }
+        mx <- x$get()
+        i <- solve(mx, ...) #calculate inverse
+        x$setInverse(i) #Return inverse matrix of x
+        i
 }
